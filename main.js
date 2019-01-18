74c24a52cf0e06dd03d07b50cf6e420a45be45bc
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-18 13:40:36 -0800

    Update to add styles to user inputs
    Instead of using greeter.textContent we can use greeter.innerHTML to change the
    text and markup!

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 function madLib() {
     var nounOneInput = document.getElementById('noun-1');
-    var nounTwoInput = document.getElementById('noun-2');
-    var numberInput = document.getElementById('number');
-    var verbInput = document.getElementById('verb');
+    // var nounTwoInput = document.getElementById('noun-2');
+    // var numberInput = document.getElementById('number');
+    // var verbInput = document.getElementById('verb');
 
     var nounOne = nounOneInput.value;
-    var nounTwo = nounTwoInput.value;
-    var number = numberInput.value;
-    var verb = verbInput.value;
+    // var nounTwo = nounTwoInput.value;
+    // var number = numberInput.value;
+    // var verb = verbInput.value;
 
-    var greeting = `When bootcamp is over, I'm going to have fun at ${nounOne}. What do you think I should have for a ${nounTwo}? How many ${number} do you think will ${verb}?`;
+    // nounOne.className = 'input';
+    // var bool = nounOne.hasAttribute('class');
+    // console.log(bool);
+
+    // var greeting = 'When bootcamp is over, I am going to have fun at ' + '<span class="input">' + nounOne + '</span>' + '. What do you think I should have for a ' + nounTwo + '? How many ' + number + ' do you think will ' + verb + '?';
+    
+    // var greeting = `When bootcamp is over, I am going to have fun at <span class="input"> ${nounOne} </span>`;
 
     var greeter = document.getElementById('greeter');
-    greeter.textContent = greeting;
+    // greeter.textContent = greeting;
+    greeter.innerHTML = 'the cow jumped over the <strong>' + nounOne + '</strong>.';
+
+    // `When bootcamp is over, I am going to have fun at <span class="input"> ${nounOne} </span>`;
     
-    console.log(greeting);
 }
